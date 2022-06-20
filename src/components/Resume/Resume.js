97939e9cdfa9820eafe27e1a451bb0ffe3f04698
masprog2022 <masprog2022@gmail.com>
2022-06-20 01:23:28 +0100

    chore: adicionando novas features na componente resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,3 +1,7 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ImgEducation from '../../assets/Resume/education.svg'
+import ImgSkills from '../../assets/Resume/programming-skills.svg'
 import './Resume.css'
 
 const divStyle = {
@@ -7,8 +11,8 @@ const divStyle = {
 
 export default function Resume() {
   return (
-    <div className="about-me-container" style={{ divStyle }}>
-      <div className="about-me-parent">
+    <div className="resume-container" style={{ divStyle }}>
+      <div className="resume-parent">
         <div className="heading-container">
           <div className="screen-heading">
             <span>Resume</span>
@@ -20,6 +24,199 @@ export default function Resume() {
             <div className="separator-line"></div>
             <div className="separator-blob">
               <div></div>
+            </div>
+          </div>
+        </div>
+        <div className="resume-card">
+          <div className="resume-card-content">
+            <div className="resume-card-content-title">
+              <img src={ImgEducation} alt="" className="imgEducation" />
+              <span>Education e Certification </span>
+            </div>
+            <div className="resume-card-content-box">
+              <div className="box-group">
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>HTML</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Basic</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>CSS</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Advanced</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>Javascript</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-group">
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>Bootstrap</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>Git</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>React</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="resume-card-content">
+            <div className="resume-card-content-title">
+              <img src={ImgSkills} alt="" className="imgEducation" />
+              <span>Programing Skills</span>
+            </div>
+            <div className="resume-card-content-box">
+              <div className="box-group">
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>HTML</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Basic</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>CSS</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Advanced</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>Javascript</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-group">
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>Bootstrap</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>Git</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+
+                <div className="box-group-date">
+                  <FontAwesomeIcon
+                    className="card-hamburger-box"
+                    icon={faBars}
+                    color="#ff5823"
+                  />
+                  <div className="resume-card-content-name">
+                    <span>React</span>
+                  </div>
+                  <div className="resume-card-content-level">
+                    <span>Intermediate</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
